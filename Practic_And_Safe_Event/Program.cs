fb286103761e4f7d5ba2cd89148e1a8245dcf468
RomanovskiiArsenii <romanovskyway@gmail.com>
2023-09-20 19:27:58 +0400

    Добавьте файлы проекта.

---
+++ Practic_And_Safe_Event/Program.cs
@@ -0,0 +1,34 @@
+﻿internal sealed class Program
+{
+    public class Publisher
+    {
+        //создание события с исключением возможности возникновения NullReferenceException
+        //указатель заполнения типом определяет тип входного параметра
+        //если требуется выходной параметр, Action следует заменить на Func
+        public event Action<int> OnCall = delegate { };
+
+        //метод вызова события
+        public void Call(int arg)
+        {
+            OnCall.Invoke(arg);
+        }
+    }
+    public class Subscriber
+    {
+        // метод-обработчик события
+        public void OnCallHandler(int arg)
+        {
+            Console.WriteLine($"Handler from subscriber, input arg: {arg}");
+        }
+    }
+    static void Main(string[] args)
+    {
+
+        Publisher publisher = new Publisher();          //создаем экземпляр издателя
+        Subscriber subscriber = new Subscriber();       //создаем экземпляр подписчика, в конструкторе подписываемся на событие
+
+        publisher.OnCall += subscriber.OnCallHandler;
+        publisher.Call(42);                             //Handler from subscriber, arg: 42
+        publisher.OnCall -= subscriber.OnCallHandler;
+    }
+}
